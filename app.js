d39ac6825b823c72dd3ff4593d375829673f70e2
Vinicius da Silva Gomes <Viniciusgomesivv@gmail.com>
2022-10-18 21:05:35 -0300

    programa para calculo de media

---
+++ app.js
@@ -0,0 +1,11 @@
+//console.log("Olá mundo")
+
+let nome = "Vinicius"
+let nota1 = 9.43
+let nota2 = 7.78
+let nota3 = 4.66
+let nota4 = 5.77
+let media = (nota1 + nota2 + nota3 + nota4) /4
+
+console.log("olá " + nome)
+console.log("sua média foi: " + media)
